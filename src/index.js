cecdcedc568daab2b6bc41a709af63c87e032f36
LenaMaistrenko <istlauder@gmail.com>
2023-01-12 18:05:24 +0200

    +

--- src/index.js
+++ src/index.js
@@ -35,16 +35,7 @@ function onInput() {
       countryInfo.innerHTML = '';
     });
 }
-// function countryAPI(country) {
-//   return fetch(
-//     `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,languages,flags`
-//   ).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-//     return response.json();
-//   });
-// }
+
 function outputCountries(data) {
   const markup = data
     .map(country => {
